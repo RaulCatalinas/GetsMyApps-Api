7b1fc2e8712fe665e8eaad6ea216a08b54c0c3de
Raul Catalinas <raulcatalinas@gmail.com>
2026-01-22 23:31:22 +0100

    chore: update env.ts file

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
 
+// eslint-disable-next-line @typescript-eslint/prefer-destructuring
 export const { TURSO_AUTH_TOKEN, TURSO_DATABASE_URL, PORT } = process.env
 export const DEFAULT_PORT = 3000
