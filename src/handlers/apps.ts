e989a8ed9e3130b043101c4581d7bf9cae511a18
Raul Catalinas <raulcatalinas@gmail.com>
2026-01-23 01:30:25 +0100

    feat: add error handling

--- src/handlers/apps.ts
+++ src/handlers/apps.ts
@@ -7,23 +7,35 @@ import { appsSchema } from '../db/schema'
 import type { Request, Response } from 'express'
 
 export async function handleIndexRoute(_: Request, res: Response) {
-  const apps = await db.select().from(appsSchema).all()
+  try {
+    const apps = await db.select().from(appsSchema)
 
-  return res.json(apps)
+    return res.json(apps)
+  } catch (error) {
+    console.error('Error fetching apps: ', error)
+
+    return res.status(500).json({ error: 'Internal server error' })
+  }
 }
 
 export async function handleGetAppByIdRoute(req: Request, res: Response) {
-  const id = Number(req.params.id)
+  try {
+    const id = Number(req.params.id)
 
-  if (Number.isNaN(id)) {
-    return res.status(400).json({ error: 'Invalid ID' })
+    if (Number.isNaN(id)) {
+      return res.status(400).json({ error: 'Invalid ID' })
+    }
+
+    const app = await db.select().from(appsSchema).where(eq(appsSchema.id, id))
+
+    if (app.length === 0) {
+      return res.status(404).json({ error: 'App not found' })
+    }
+
+    return res.json(app[0])
+  } catch (error) {
+    console.error('Error fetching app by ID: ', error)
+
+    return res.status(500).json({ error: 'Internal server error' })
   }
-
-  const app = await db.select().from(appsSchema).where(eq(appsSchema.id, id))
-
-  if (app.length === 0) {
-    return res.status(404).json({ error: 'App not found' })
-  }
-
-  return res.json(app[0])
 }
