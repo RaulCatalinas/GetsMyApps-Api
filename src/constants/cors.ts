dcdcb19369b526edf228d3b3e61efc8211df52b4
Raul Catalinas <raulcatalinas@gmail.com>
2026-01-27 00:24:17 +0100

    feat: configured cors

---
+++ src/constants/cors.ts
@@ -0,0 +1,10 @@
+export const ALLOWED_ORIGINS = [
+  // Production
+  'https://raulcatalinas.vercel.app',
+  'https://raulcatalinas.vercel.app/es',
+  'https://gets-my-apps.vercel.app/',
+  'https://gets-my-apps.vercel.app/es',
+
+  // Local development
+  'http://localhost:4321'
+]
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,29 @@
 import appsRouter from './routes/apps'
 
 // Constants
+import { ALLOWED_ORIGINS } from './constants/cors'
 import { DEFAULT_PORT, PORT } from './constants/env'
 
 // Third-Party libraries
+import cors from 'cors'
 import express from 'express'
 
 const app = express()
 
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (origin === undefined || ALLOWED_ORIGINS.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  })
+)
 app.use(express.json())
 app.use(appsRouter)
 
